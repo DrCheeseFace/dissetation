7116ed92081c91c858a8a8801922f2200ff7a4fd
tharun11 <tharun1@hotmail.co.uk>
2026-03-02 17:29:58 +0000

    feat: basic missing data generator for heart-disease dataset

---
+++ synthetic_missing_data/missing-maker-heart-disease.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import random
+import numpy as np
+
+IN_DATASOURCE_PATH = "./datasets/heart-disease.csv"
+OUT_MCAR_DATASOURCE_PATH = "./out_datasets/MCAR-heart-disease.csv"
+OUT_MAR_DATASOURCE_PATH = "./out_datasets/MAR-heart-disease.csv"
+
+RANDOM_SEED = 99
+np.random.seed(RANDOM_SEED)
+random.seed(RANDOM_SEED)
+
+base_dataframe = pd.read_csv(IN_DATASOURCE_PATH)
+# change "Heart Disease" to boolean
+base_dataframe["Heart Disease"] = base_dataframe["Heart Disease"] == "Presence"
+
+# drop id column
+base_dataframe = base_dataframe.drop(columns=['id'])
+
+# ---
+
+# create MCAR dataset
+MCAR_dataframe = base_dataframe.copy().astype(object)
+
+MCAR_RATE = 0.05
+mask = np.random.rand(*MCAR_dataframe.shape) < MCAR_RATE
+MCAR_dataframe = MCAR_dataframe.mask(mask)
+
+MCAR_dataframe.to_csv(OUT_MCAR_DATASOURCE_PATH, index=False)
+
+# ---
+
+# create MAR dataset
+# if age > 60: 33% chance of "Heart Disease" missing
+MAR_dataframe = base_dataframe.copy().astype(object)
+
+MAR_RATE = 0.33
+condition = MAR_dataframe['Age'] > 60
+random_draw = np.random.rand(len(MAR_dataframe)) < MAR_RATE
+mar_mask = condition & random_draw
+MAR_dataframe.loc[mar_mask, "Heart Disease"] = np.nan
+
+MAR_dataframe.to_csv(OUT_MAR_DATASOURCE_PATH, index=False)
---
+++ visu/todo.md
@@ -0,0 +1,15 @@
+- mobx stores
+- routes.ts
+- enving it up for frontend
+- dockerise everything
+
+- creating synthetic missing data type examples
+    - Joint Missingness (JM)
+    - Conditional Missingness (CM)
+    ---
+    - Missing Completely at Random (MCAR)
+    - Missing at Random (MAR)
+    - Missing Not at Random (MNAR)
+
+    - https://www.kaggle.com/datasets/algozee/heart-decices attempting on this
+
