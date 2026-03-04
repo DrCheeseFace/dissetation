708cd456fa01c61b64cec541cbae785eec91147e
tharun11 <tharun1@hotmail.co.uk>
2026-03-04 14:53:17 +0000

    chore: added source

--- synthetic_missing_data/missing-maker-heart-disease.py
+++ synthetic_missing_data/missing-maker-heart-disease.py
@@ -5,6 +5,7 @@ import numpy as np
 IN_DATASOURCE_PATH = "./datasets/heart-disease.csv"
 OUT_MCAR_DATASOURCE_PATH = "./out_datasets/MCAR-heart-disease.csv"
 OUT_MAR_DATASOURCE_PATH = "./out_datasets/MAR-heart-disease.csv"
+OUT_MNAR_DATASOURCE_PATH = "./out_datasets/MNAR-heart-disease.csv"
 
 RANDOM_SEED = 99
 np.random.seed(RANDOM_SEED)
@@ -22,9 +23,9 @@ base_dataframe = base_dataframe.drop(columns=['id'])
 # create MCAR dataset
 MCAR_dataframe = base_dataframe.copy().astype(object)
 
-MCAR_RATE = 0.05
-mask = np.random.rand(*MCAR_dataframe.shape) < MCAR_RATE
-MCAR_dataframe = MCAR_dataframe.mask(mask)
+MCAR_RATE = 0.33
+random_draw = np.random.rand(len(MCAR_dataframe)) < MCAR_RATE
+MCAR_dataframe.loc[random_draw, "Heart Disease"] = np.nan
 
 MCAR_dataframe.to_csv(OUT_MCAR_DATASOURCE_PATH, index=False)
 
@@ -41,3 +42,18 @@ mar_mask = condition & random_draw
 MAR_dataframe.loc[mar_mask, "Heart Disease"] = np.nan
 
 MAR_dataframe.to_csv(OUT_MAR_DATASOURCE_PATH, index=False)
+
+# ---
+
+# create MNAR dataset
+# if "Heart Disease" == true; 33% chance of being missing
+
+MNAR_dataframe = base_dataframe.copy().astype(object)
+
+MNAR_RATE = 0.33
+condition = MNAR_dataframe["Heart Disease"]
+random_draw = np.random.rand(len(MNAR_dataframe)) < MAR_RATE
+mar_mask = condition & random_draw
+MAR_dataframe.loc[mar_mask, "Heart Disease"] = np.nan
+
+MAR_dataframe.to_csv(OUT_MNAR_DATASOURCE_PATH, index=False)
